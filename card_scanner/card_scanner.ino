c49a82bb53993dd09de06e629870a83654cd87d8
Vlada <vladap@mit.edu>
2022-04-20 02:01:07 -0400

    integrate with servo

--- card_scanner/card_scanner.ino
+++ card_scanner/card_scanner.ino
@@ -12,30 +12,85 @@
 #define SS_PIN 34
 #define RST_PIN 20
 MFRC522 mfrc522(SS_PIN, RST_PIN);
- 
+
+const int RED_LED = 2;
+const int GREEN_LED = 3;
+const int BLUE_LED = 4;
+int pos;
+int pos_min = int(0.05*1024);
+int pos_max = int(0.1*1024);
+int servoPin = 21;
+int buttonPin = 45;
+int pwm_channel = 0;
+bool doorOpen = false;
+bool buttonPressed = false;
+bool toClose = false;
+int pinMeasurement = -1;
+bool accessAuthorized = false;
+char authorizedCard[1000] = "63 E1 B3 31";
+
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  ledcSetup(pwm_channel, 50, 10);
+  ledcAttachPin(servoPin, pwm_channel);
+  pinMode(buttonPin,INPUT_PULLUP);
   SPI.begin();      
   mfrc522.PCD_Init();
+  pinMode(RED_LED,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
+  pinMode(BLUE_LED,OUTPUT);
+  digitalWrite(RED_LED, 1);
+  digitalWrite(BLUE_LED, 1);
+  digitalWrite(GREEN_LED, 1);
   Serial.println("Approximate your card to the reader...");
-  Serial.println();
-
+  Serial.println();  
 }
 
 
 void loop() 
 {
+  manage_close_button();
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
+  if(mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
+    read_card();
   }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
+} 
+
+void operate_door(){
+  if(toClose && doorOpen){
+    for (pos = pos_max; pos >= pos_min; pos -= 1) { // goes from 180 degrees to 0 degrees
+      ledcWrite(pwm_channel, pos);    // tell servo to go to position in variable 'pos'
+      delay(50);             // waits 15ms for the servo to reach the position
+    }
+    doorOpen = !doorOpen;
   }
+  if(accessAuthorized && !doorOpen){
+    for (pos = pos_min; pos <= pos_max; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      ledcWrite(pwm_channel, pos);    // tell servo to go to position in variable 'pos'
+      delay(50);             // waits 15ms for the servo to reach the position
+    }
+    doorOpen = !doorOpen;
+  }
+  toClose = false;
+}
+
+void manage_close_button(){
+  pinMeasurement = digitalRead(buttonPin);
+  if (pinMeasurement == 0){
+    buttonPressed = true;
+    digitalWrite(BLUE_LED, 0);
+    toClose = true;
+    operate_door();
+  }
+  else{
+    digitalWrite(BLUE_LED, 1);
+    buttonPressed = false;
+  }
+}
+
+void read_card(){
   //Show UID on serial monitor
   Serial.print("UID tag :");
   String content= "";
@@ -63,15 +118,22 @@ void loop()
   memcpy(newcard, &cardcontent[1], strlen(cardcontent)-1);
   Serial.println(newcard);
 
-  if(!strcmp(newcard, "E0 16 99 1B"))
+  if(!strcmp(newcard, authorizedCard))
   {
     Serial.println("Authorized access");
     Serial.println();
-    delay(3000);
+    digitalWrite(GREEN_LED, 0);
+    accessAuthorized = true;
+    operate_door();
+    accessAuthorized = false;
+    delay(1000);
+    digitalWrite(GREEN_LED, 1);
   }
  
- else{
+  else{
     Serial.println(" Access denied");
+    digitalWrite(RED_LED, 0);
     delay(3000);
+    digitalWrite(RED_LED, 1);
   }
-} 
+}
