b3d9988d505fcda2a4a67f1269128f0103f05870
Kasra Mazaheri <mazaheri@mit.edu>
2022-04-27 19:10:05 -0400

    Admin Page is now only accessible by admins who have logged in

--- server_src/admin.py
+++ server_src/admin.py
@@ -2,10 +2,19 @@ import sqlite3
 import sys
 
 sys.path.append('/var/jail/home/team26/server_src/')
-from database_request import get_all_users, get_user_door_access_input, add_door_access, remove_door_access
+from database_request import get_id, get_all_users, get_user_door_access_input, add_door_access, remove_door_access, get_user_info_from_session, check_admin
 
 
 def request_handler(request):
+
+    user_hash = get_id()
+    user_info = get_user_info_from_session(user_hash)
+    if user_info:
+        username, password = user_info[0], user_info[1]
+        if not check_admin(username): # User does not have admin privileges, unauthorized access
+            return "unauthorized access"
+    else: # User is not logged in, unauthorized access
+        return "unauthorized access"
 
     if request["method"] == "POST" and "admin" not in request["form"]:
         username = request["form"]["username"]
